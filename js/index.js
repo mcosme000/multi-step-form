4d609896861e3b2aa9ecad1cfd52bf3340f59c81
Maria Cosmen <mcosme00@gmail.com>
2023-06-13 22:40:45 +0900

    Set selected step

--- js/index.js
+++ js/index.js
@@ -1,23 +1,21 @@
 let currentStep = "1"
 
 const tabs = document.querySelectorAll(".tab")
-const steps = document.querySelectorAll(".step")
+const steps = document.querySelectorAll(".circle")
 
 const forwardButtons = document.querySelectorAll(".btn")
 const backButtons = document.querySelectorAll(".btn-back")
 
-// DOM ELEMENTS
-
 const updateCurrentStep = (number, type) => {
-  // hide the current step
   tabs[number - 1].style.display = 'none'
+  steps[number - 1].classList.remove('selected')
 
   if (type.includes('next')) {
-    // display the next one
     tabs[number].style.display = 'block'
-  } else {
-    // display the previous one
+    steps[number].classList.add('selected')
+  } else if (type.includes('back')) {
     tabs[number - 2].style.display = 'block'
+    steps[number - 2].classList.add('selected')
   }
 }
 
