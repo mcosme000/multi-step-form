c43edf66a4cf05aacb75a9ca51978b1e40b43dc7
Maria Cosmen <mcosme00@gmail.com>
2023-06-14 15:07:53 +0900

    Create object with form inputs

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
-let currentStep = "1"
-
+const links = document.querySelectorAll("a");
 const tabs = document.querySelectorAll(".tab")
 const steps = document.querySelectorAll(".circle")
-
-const forwardButtons = document.querySelectorAll(".btn")
-const backButtons = document.querySelectorAll(".btn-back")
 
 const updateCurrentStep = (number, type) => {
   tabs[number - 1].style.display = 'none'
   steps[number - 1].classList.remove('selected')
 
-  if (type.includes('next')) {
+  if (type.includes('next') || type.includes('confirm')) {
     tabs[number].style.display = 'block'
     steps[number].classList.add('selected')
   } else if (type.includes('back')) {
@@ -19,13 +15,7 @@ const updateCurrentStep = (number, type) => {
   }
 }
 
-forwardButtons.forEach((button) => button.addEventListener('click', (e) => {
-  let className = e.target.getAttribute('class')
-  let selectedStep = e.target.parentElement.parentElement.parentElement.getAttribute('step')
-  updateCurrentStep(selectedStep, className)
-}))
-
-backButtons.forEach((button) => button.addEventListener('click', (e) => {
+links.forEach((button) => button.addEventListener('click', (e) => {
   let className = e.target.getAttribute('class')
   let selectedStep = e.target.parentElement.parentElement.parentElement.getAttribute('step')
   updateCurrentStep(selectedStep, className)
@@ -40,6 +30,71 @@ const yearlyDiscounts = document.querySelectorAll('.yearly-discount');
 const monthlyAddonPrices = document.querySelectorAll('.monthly-addon')
 const yearlyAddonPrices = document.querySelectorAll(".yearly-addon")
 
+let information = {}
+
+// Display final information
+const updateInformation = () => {
+  console.log(information)
+  let confirmAddOn = document.getElementById('confirm-addon')
+  while (confirmAddOn.firstChild) {
+    confirmAddOn.removeChild(confirmAddOn.firstChild)
+  }
+
+  if ('Plan' in information) {
+    let plan = information['Plan']
+    document.getElementById("selected-plan").innerHTML = plan.planName
+    document.getElementById("selected-price").innerHTML = plan.planPrices[0].innerHTML
+  }
+
+  if ('Addons' in information) {
+    information['Addons'].forEach(addon => {
+      let div = document.createElement('DIV')
+      div.classList = "addon flex"
+
+      let p = document.createElement('p')
+      p.innerHTML = addon[0]
+
+      let price = document.createElement('p')
+      price.innerHTML = addon[1][0].innerHTML
+
+      div.appendChild(p)
+      div.appendChild(price)
+      confirmAddOn.appendChild(div)
+    })
+
+  } else if (information['Addons'] === []) {
+    let p = document.createElement('p')
+    p.innerHTML = 'No add-ons included'
+    console.log(p)
+    confirmAddOn.appendChild(p)
+  }
+}
+
+// Gather information
+document.querySelectorAll("input[name='plan']").forEach(input => {
+  input.addEventListener('change', () => {
+    document.querySelectorAll("input[name='plan']:checked").forEach(selectedOption => {
+      let planName = selectedOption.getAttribute('id')
+      let planPrices = selectedOption.nextElementSibling.querySelectorAll('.plan-price')
+      information['Plan'] = { planName, planPrices }
+    })
+    updateInformation();
+  })
+})
+
+document.querySelectorAll("input[name='addon']").forEach(option => {
+  option.addEventListener('change', (e) => {
+    let addons = []
+    document.querySelectorAll("input[name='addon']:checked").forEach(selectedOption => {
+      const selectedTarget = selectedOption.nextElementSibling;
+      let option = selectedTarget.querySelector(".title").innerText;
+      let optionPrices = selectedTarget.querySelectorAll(".addon-price");
+      addons.push([option, optionPrices]);
+    });
+    information['Addons'] = addons
+    updateInformation();
+  })
+})
 
 yearlyInput.addEventListener('change', function() {
   yearlyDiscounts.forEach(discount => {
